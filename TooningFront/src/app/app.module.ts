5ba24d30b6c8cc6021dc5199d60ee43e4c02d06b
panggu9026 <patmat1004@gmail.com>
2023-01-17 09:05:30 +0900

    이미지 base64 인코딩 디코딩

--- TooningFront/src/app/app.module.ts
+++ TooningFront/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule,Routes} from '@angular/router'; //라우터
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ParticipatingWorksComponent } from './participating-works/participating-works.component';
 import { ParticipatingComponent } from './participating/participating.component';
@@ -29,7 +29,8 @@ const router : Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(router,{enableTracing:false}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TooningFront/src/app/participating-works/participating-works.component.ts
+++ TooningFront/src/app/participating-works/participating-works.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, subscribeOn, Subscriber } from 'rxjs';
+
 
 @Component({
   selector: 'app-participating-works',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./participating-works.component.css']
 })
 export class ParticipatingWorksComponent {
+myImage!:Observable<any>;
+
+base64code!: any
+onChange = ($event: Event)=>{
+  const target = $event.target as HTMLInputElement;
+
+const file : File = (target.files as FileList)[0];
+console.log(file)
+
+this.convertToBase64(file)
+}
+convertToBase64(file:File){
+ const observable = new Observable((subscriber:Subscriber<any>) =>{
+ this.readFile(file,subscriber)
+ })
+
+ observable.subscribe((d) => {
+  console.log(d)
+  this.myImage = d
+  this.base64code = d
+ })
+}
+readFile(file: File, subscriber: Subscriber<any>){
+  const filereader = new FileReader();
+
+  filereader.readAsDataURL(file)
+
+  filereader.onload = () => {
+    subscriber.next(filereader.result);
+
+    subscriber.complete()
+  }
+  filereader.onerror = () => {
+    subscriber.error()
+    subscriber.complete()
+  }
+}
+
 
 }
